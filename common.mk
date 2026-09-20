3bd5e450ca34ae79397eee67837b4fb2902d7008
Panjol-star <panjol-star@user.noreply.github.com>
2026-09-20 15:21:00 +0700

    Revert SQUASH: pembatalan commit

--- common.mk
+++ common.mk
@@ -369,7 +369,3 @@ PRODUCT_COPY_FILES += \
 
 # Inherit from vendor blobs
 $(call inherit-product, vendor/motorola/sm6375-common/sm6375-common-vendor.mk)
-
-# Dolby
-# https://github.com/crdroidandroid/android_hardware_dolby.git branch LunarisDolby
-$(call inherit-product, hardware/dolby/dolby.mk)
